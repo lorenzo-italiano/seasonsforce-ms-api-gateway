30f20396fe06bfff967cbd84bcac7f772ef95abf
romainfrezier <romainfrezier@gmail.com>
2023-10-12 16:40:18 +0200

    feat(login): Adding a login route that redirect to the keycloak server.

--- src/main/java/fr/polytech/GatewayConfig.java
+++ src/main/java/fr/polytech/GatewayConfig.java
@@ -11,7 +11,13 @@ public class GatewayConfig {
     @Bean
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
         return builder.routes()
-                //TODO fix api route and then this route
+                // TODO: fix api route and then this route
+                // TODO: english comments
+                .route("keycloak-route", r -> r
+                        .path("/api/v1/auth/login")  // Le chemin de la requête à matcher
+                        .filters(f -> f.rewritePath("/api/v1/auth/login", "/realms/seasonsforce/protocol/openid-connect/token"))
+                        .uri("http://keycloak-server:8080")  // L'URI de destination du service
+                )
                 .route("user-route", r -> r
                         .path("/api/v1/users/**") // Le chemin de la requête à matcher
                         .uri("lb://offers-api") // L'URI de destination du service
