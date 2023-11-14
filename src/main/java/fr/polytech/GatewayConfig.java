1a20e16907f898606934985526a390116b20d28e
romainfrezier <romainfrezier@gmail.com>
2023-11-14 13:51:23 +0100

    feat(new routes): Adding new routes for review.

--- src/main/java/fr/polytech/GatewayConfig.java
+++ src/main/java/fr/polytech/GatewayConfig.java
@@ -70,6 +70,10 @@ public class GatewayConfig {
                         .path("/api/v1/plan/**") // Path of the request to match
                         .uri("lb://payment-api") // Destination URI of the service
                 )
+                .route("review-route", r -> r
+                        .path("/api/v1/review/**") // Path of the request to match
+                        .uri("lb://review-api") // Destination URI of the service
+                )
                 .build();
     }
 
